c508cd5e0c5d89f8a19cdd71045565240ce5d79e
dtaylor0 <drewtaylorm@gmail.com>
2020-10-28 20:47:53 -0400

    GUI progress

--- project3/src/sample/Controller.java
+++ project3/src/sample/Controller.java
@@ -1,9 +1,25 @@
 package sample;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.VBox;
+
+import java.io.IOException;
 
 public class Controller {
-    public void pressButton(ActionEvent event) {
-        System.out.println("hello there");
+
+    @FXML
+    private BorderPane bp;
+
+
+    @FXML
+    public void openAccountPage(ActionEvent event) throws IOException {
+        System.out.println("hi");
+        VBox openPage = FXMLLoader.load(getClass().getResource("openAccount.fxml"));
+        bp.setCenter(openPage);
+
+
     }
 }
